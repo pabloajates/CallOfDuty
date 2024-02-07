4641c6eaacad1a5f066018d5c27d5312cc504fb8
arruchetz <dgarruche@gmail.com>
2024-02-07 17:19:09 +0100

    Main Base

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -1,7 +1,55 @@
 package presentation;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        int option= 0;
+
+        do {
+            System.out.println("------------------------------");
+            System.out.println("|       MENU PRINCIPAL       |");
+            System.out.println("------------------------------");
+            System.out.println("| 1. Open inventory          |");
+            System.out.println("| 2. Add a weapon            |");
+            System.out.println("| 3. Add accessories         |");
+            System.out.println("| 4. Equip on character      |");
+            System.out.println("| 5. Exit                    |");
+            System.out.println("------------------------------");
+            System.out.println("|  Write the option number.  |");
+            System.out.println("------------------------------");
+
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+                    exit = true;
+                    break;
+
+
+            }
+
+        }while(exit);
+
+
     }
+
 }
